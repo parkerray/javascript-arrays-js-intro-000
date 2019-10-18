152f9cd2f71ec93b3c3c9103f973d949dba7b6db
Parker Thompson <park.thompson@gmail.com>
2019-10-18 01:02:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,1 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray(array[],element) {
-  array[].unshift(element)
-  return array[]
-}
-
-function destructivelyAddElementToBeginningOfArray(array,element) {
-
-}
+const chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
