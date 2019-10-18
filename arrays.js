0be12ae24a112d6df8e4a0a04febd207ea090ae4
Parker Thompson <park.thompson@gmail.com>
2019-10-18 01:27:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function removeElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
