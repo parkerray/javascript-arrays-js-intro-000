e51859a09112f23e68b6c01a0e4a3bda72843106
Parker Thompson <park.thompson@gmail.com>
2019-10-18 01:20:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = array;
-  newArray.shift(element);
+  var newArray = [...array, element];
   return newArray;
 }
 
