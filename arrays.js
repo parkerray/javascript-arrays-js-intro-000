81fc30e5c8396c198b5ac04b5e2ee8c0ebe37bd9
Parker Thompson <park.thompson@gmail.com>
2019-10-18 00:50:03 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array[],element) {
+  array[].unshift(element)
+  return array[]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  
+}
